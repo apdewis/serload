65b70841a7536b024cab0d08437c3b3e74552eb1
Adam Dewis <adam.dewis@gmail.com>
2019-06-05 21:29:28 +1000

    changes for cross platform compat (win, linux)

--- serload.c
+++ serload.c
@@ -2,33 +2,37 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <termios.h>
+//#include <termios.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <strings.h>
+//#include <unistd.h>
+//#include <strings.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <argparse.h>
+#include <c_serial.h>
 
 #include "commands.h"
   
-#define BAUDRATE B921600
+#define BAUDRATE 921600
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define DEFAULT_BASE 0x40000000
-#define BLOCK_SIZE 655536
+#define BLOCK_SIZE 65536
 
 uint32_t base = DEFAULT_BASE;
 int serdev;
 uint8_t buf[BLOCK_SIZE];
 int fd, c, res;
 FILE *fd_data;
-struct termios oldtio,newtio;
+int status;
 const char *port = NULL;
 const char *file = NULL;
 uint8_t *run = NULL;
 struct stat st;
 uint8_t *file_data;
 uint8_t revert_tc = 0;
+int32_t data_length = 0;
+c_serial_port_t* m_port;
+c_serial_control_lines_t m_lines;
 
 static const char *const usage[] = 
 {
@@ -38,7 +42,6 @@ static const char *const usage[] =
 
 void terminate(int code)
 {
-    if(revert_tc) tcsetattr(fd,TCSANOW,&oldtio);
     if(file_data)
     {
         free(file_data);
@@ -46,13 +49,20 @@ void terminate(int code)
     return code;
 }
 
-int8_t send(uint8_t *buf, uint32_t len)
+int8_t cmd_send(uint8_t *buf, uint32_t len)
 {
     int32_t res;
-    uint8_t rsp;
-    res = write(serdev,buf,len);
-    res = read(serdev,&rsp,1);
-    if(rsp == ACK)
+    data_length = len;
+    res = c_serial_write_data(m_port, buf, &data_length);
+    if(res < 0) return -1;
+
+    data_length = 1;
+    do 
+    {
+        res = c_serial_read_data(m_port, buf, &data_length, &m_lines);
+    } while(res = 0);
+
+    if(buf[0] == ACK)
     {
         return 0;
     }
@@ -62,12 +72,14 @@ int8_t send(uint8_t *buf, uint32_t len)
     }
 }
 
-int8_t recv(uint8_t *rsp, uint32_t len)
+int8_t cmd_recv(uint8_t *buf, uint32_t len)
 {
     int32_t res;
+    data_length = 1;
     for(uint8_t count = 0; count < len; count++)
     {
-        res = read(serdev,&rsp[count],1);
+        res = c_serial_read_data(m_port, &buf[count], &data_length, &m_lines );
+        //res = read(serdev,&buf[count],1);
     }
 
     return 0;
@@ -117,30 +129,55 @@ int process_args(int argc, const char **argv)
 
 void init()
 {
-    serdev = open(port, O_RDWR | O_NOCTTY  ); 
-    if (serdev <0) {perror(port); exit(-1); }
+    //serdev = open(port, O_RDWR | O_NOCTTY  ); 
+    //if (serdev <0) {perror(port); exit(-1); }
     
     fd_data = fopen(file, "rb");
     if (fd_data == NULL) {perror("FILE"); exit(-1); }
 
     if (stat(file, &st) != 0) {perror("ST"); exit(-1); }
 
-    tcgetattr(fd,&oldtio); /* save current port settings */
-    
-    bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
-    
-    /* set input mode (non-canonical, no echo,...) */
-    newtio.c_lflag = 0;
-    
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
-    
-    tcflush(fd, TCIFLUSH);
-    tcsetattr(fd,TCSANOW,&newtio);
-    revert_tc = 1;
+    //tcgetattr(fd,&oldtio); /* save current port settings */
+    //
+    //bzero(&newtio, sizeof(newtio));
+    //newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    //newtio.c_iflag = IGNPAR;
+    //newtio.c_oflag = 0;
+    //
+    ///* set input mode (non-canonical, no echo,...) */
+    //newtio.c_lflag = 0;
+    //
+    //newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    //newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+    //
+    //tcflush(fd, TCIFLUSH);
+    //tcsetattr(fd,TCSANOW,&newtio);
+    //revert_tc = 1;
+
+    if( c_serial_new( &m_port, NULL ) < 0 ){
+        fprintf( stderr, "ERROR: Unable to create new serial port\n" );
+        exit(-1);
+    }
+
+    if( c_serial_set_port_name(m_port, port) < 0 ) {
+        fprintf( stderr, "ERROR: can't set port name\n" );
+    }
+
+    c_serial_set_baud_rate(m_port, BAUDRATE);
+    c_serial_set_data_bits(m_port, CSERIAL_BITS_8);
+    c_serial_set_stop_bits(m_port, CSERIAL_STOP_BITS_1);
+    c_serial_set_parity(m_port, CSERIAL_PARITY_NONE);
+    c_serial_set_flow_control(m_port, CSERIAL_FLOW_NONE);
+
+    c_serial_set_serial_line_change_flags( m_port, CSERIAL_LINE_FLAG_ALL );
+
+    status = c_serial_open( m_port );
+    if( status < 0 ){
+        fprintf( stderr, "ERROR: Can't open serial port\n" );
+        exit(-1);
+    }
+
+    printf("opened port \n\r");
 }
 
 int send_block(uint8_t *data, uint32_t dest_addr, uint32_t len)
@@ -149,29 +186,35 @@ int send_block(uint8_t *data, uint32_t dest_addr, uint32_t len)
     int count = 0;
     send_buf[0] = CMD_WRITE;
 
-    if (send(send_buf, 1) != 0) 
+    if (cmd_send(send_buf, 1) != 0) 
     {
         printf("invalid response\n\r");
         return -1;
     }
     
     wordToBytes(send_buf, dest_addr);
-    if (send(send_buf, 4) != 0) 
+    if (cmd_send(send_buf, 4) != 0) 
     {
         printf("invalid response\n\r");
         return -1;
     }
 
     wordToBytes(send_buf, len);
-    if (send(send_buf, 4) != 0)
+    if (cmd_send(send_buf, 4) != 0)
     {
         printf("invalid response\n\r");
         return -1;
     }
 
-    if(send(data, len) != 0)
+    if(cmd_send(data, len) != 0)
     {
+        uint8_t err_buf[9];
         printf("Send error\n");
+        cmd_recv(err_buf, 9);
+        uint8_t exc = err_buf[0];
+        uint32_t epc = err_buf[1];
+        uint32_t mtval = err_buf[4];
+        printf("err: %x %x", exc, epc);
         return -1;
     }
 
@@ -198,7 +241,7 @@ int main(int argc, const char **argv)
     
     printf("Waiting for device\n\r");
     buf[0] = CMD_CHKRDY;
-    if (send(buf, 1) != 0)
+    if (cmd_send(buf, 1) != 0)
     { 
         printf("invalid response\n\r");
         terminate(-1);
@@ -227,14 +270,14 @@ int main(int argc, const char **argv)
     {
         printf("Running program \n\r");
         buf[0] = CMD_JUMP;
-        if (send(buf, 1) != 0) 
+        if (cmd_send(buf, 1) != 0) 
         {
             printf("invalid response\n");
             terminate(-1);
         }
     
         wordToBytes(buf, base);
-        if (send(buf, 4) != 0) 
+        if (cmd_send(buf, 4) != 0) 
         {
             printf("invalid response\n");
             terminate(-1);
